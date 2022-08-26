8ea4e1d6be156411ab20bdf33cc80937541c0acd
HassanEmad0010 <hassanemad0010@gmail.com>
2022-08-27 00:06:18 +0200

    text form feild, validation working

--- lib/modules/NewTaskScreen.dart
+++ lib/modules/NewTaskScreen.dart
@@ -12,69 +12,76 @@ class NewTaskScreen extends StatelessWidget {
 
       body: Center(
         child: Column(
-          children:  [
-          const Text (
+          children: [
+            const Text (
               "New Tasks",
-              style: TextStyle(fontSize: 60),
-          ),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.lightBlue,
-              child: IconButton(
-                iconSize: 40,
-                icon: Icon(Icons.add),
-                onPressed: () async{
-                  print("+ pressed");
-
-                  String task="Java";
-                  String status="done";
-                 var responce= await dbObject.insertData(sqlCommand: 'INSERT INTO TASKS(TITLE,DATE,TIME,STATUS) VALUES("$task", "pray", "25-8-2022","$status")');
-                 print("Data inserted to the table ");
-                  print(responce.toString());
-
-
-                 },
-
-              ),
+              style: TextStyle(fontSize: 40),
             ),
-            const SizedBox(height: 10,),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.lightBlue,
-              child: IconButton(
-                iconSize: 40,
-                icon: Icon(Icons.visibility),
-                onPressed: () async{
-                  print("view pressed");
-                  String sqlCommand="SELECT * FROM 'TASKS'";
-                  var responce = await dbObject.readData(sqlCommand: sqlCommand);
-                  print("$responce");
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.lightBlue,
+                  child: IconButton(
+                    iconSize: 70,
+                    icon: Icon(Icons.add),
+                    onPressed: () async{
+                      print("+ pressed");
+
+                      String task="Java";
+                      String status="done";
+                     var responce= await dbObject.insertData(sqlCommand: 'INSERT INTO TASKS(TITLE,DATE,TIME,STATUS) VALUES("$task", "pray", "25-8-2022","$status")');
+                     print("Data inserted to the table ");
+                      print(responce.toString());
 
 
-                },
+                     },
 
-              ),
-            ),
-            const SizedBox(height: 10,),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.lightBlue,
-              child: IconButton(
-                iconSize: 40,
-                icon: Icon(Icons.remove_circle),
-                onPressed: () async{
-                  print("delete pressed");
-                  String sqlCommand="DELETE FROM 'TASKS' WHERE id IN (3,8,9,10)";
-                  var responce=await dbObject.DeleteData(sqlCommand: sqlCommand);
-                  print(responce);
+                  ),
+                ),
+
+                const SizedBox(width: 10,),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.lightBlue,
+                  child: IconButton(
+                    iconSize: 70,
+                    icon: Icon(Icons.visibility),
+                    onPressed: () async{
+                      print("view pressed");
+                      String sqlCommand="SELECT * FROM 'TASKS'";
+                      var responce = await dbObject.readData(sqlCommand: sqlCommand);
+                      print("$responce");
 
 
-                },
+                    },
 
-              ),
+                  ),
+                ),
+                const SizedBox(width: 10,),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.lightBlue,
+                  child: IconButton(
+                    iconSize: 70,
+                    icon: Icon(Icons.remove_circle),
+                    onPressed: () async{
+                      print("delete pressed");
+                      String sqlCommand="DELETE FROM 'TASKS' WHERE id IN (3,8,9,10)";
+                      var responce=await dbObject.DeleteData(sqlCommand: sqlCommand);
+                      print(responce);
+
+
+                    },
+
+                  ),
+                ),
+              ],
             ),
           ],
         ),
