51b9450749969ed098c5733f3d05c30c97360812
HassanEmad0010 <hassanemad0010@gmail.com>
2022-08-27 23:43:46 +0200

    handling adding tasks to new task screen, and long press to remove it to done task table,and done task screen

--- lib/modules/DoneTaskScreen.dart
+++ lib/modules/DoneTaskScreen.dart
@@ -2,115 +2,79 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../shared/componant/DataBaseClass.dart';
+import '../shared/componant/componant.dart';
 
 class DoneTaskScreen extends StatelessWidget {
   SqlDb dbObject = new SqlDb();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body:
+      Container(
+        margin: EdgeInsets.all(24),
+        //width: double.infinity,
+        //height:  200,
+        child:
+        ListView(
+          scrollDirection: Axis.vertical,
 
-        children:[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            FutureBuilder(
+              future: dbObject.readData(sqlCommand: "SELECT * FROM 'DONETASKS'"),
+              builder: (context,AsyncSnapshot<List<Map>> snapshot) {
+                if(snapshot.hasData) {
+                  return ListView.separated(
+                      separatorBuilder: (context,value)=>const SizedBox(height: 10,) ,
 
-            const SizedBox(
-              width: 10,
-            ),
-            Column(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.lightBlue,
-                  child: IconButton(
-                    iconSize: 70,
-                    icon: Icon(Icons.visibility),
-                    onPressed: () async {
-                      print("view pressed");
-                      String sqlCommand = "SELECT * FROM 'TASKS'";
-                      var responce =
-                      await dbObject.readData(sqlCommand: sqlCommand);
-                      print("$responce");
-                    },
-                  ),
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.deepPurple,
-                  child: IconButton(
-                    iconSize: 70,
-                    icon: Icon(Icons.visibility),
-                    onPressed: () async {
-                      print("view pressed");
-                      String sqlCommand = "SELECT * FROM 'DoneTASKS'";
-                      var responce =
-                      await dbObject.readData(sqlCommand: sqlCommand);
-                      print("$responce");
-                    },
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.lightBlue,
-              child: IconButton(
-                iconSize: 70,
-                icon: Icon(Icons.remove_circle),
-                onPressed: () async {
-                  print("delete pressed");
-                  String sqlCommand ="Delete FROM 'TASKS'";
-               //   var responce =
-                  //await dbObject.DeleteData(sqlCommand: sqlCommand);
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder:
+                          (context, index) {
 
-                //  print(responce);
-                },
-              ),
-            ),
+                        return cardBuilder(
+                            snapTitle: snapshot.data![index]['TITLE'],
+                            snapDate:  snapshot.data![index]['DATE'],
+                            snapStatus:  snapshot.data![index]['STATUS'],
+                            snapTime:  snapshot.data![index]['TIME'],
+                            longPressFunction: () async{
+                              print("long pressed");
+                              print("snap"+snapshot.toString());
 
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.lightBlue,
-              child: IconButton(
-                iconSize: 70,
-                icon: Icon(Icons.delete),
-                onPressed: ()  {
+                              await dbObject.insertData(tableName: 'DraftTASKS',
+                                title: snapshot.data![index]['TITLE'],
+                                date: snapshot.data![index]['DATE'],
+                                time: snapshot.data![index]['TIME'],
+                                status:snapshot.data![index]['TIME'],
+                              );
 
-                  dbObject.DeleteDataBase();
+                              await dbObject.DeleteData(
+//I HAVE ISSUE HERE, need to access data with the uniqe id
+                                rowData: snapshot.data![index]['TITLE'],
+                                tableName:'TASKS' ,
 
-                },
-              ),
+                              );
+
+                            }
+
+
+                        );
+                      }
+
+                  );
+                }else
+                {
+                  return const Center(child:  CircularProgressIndicator(color: Colors.red,value: 40),
+                    widthFactor: 50,
+                  heightFactor: 40,
+                  );
+                }
+              },
+
             ),
           ],
         ),
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        Text(
-          "Done Tasks",
-          style: TextStyle(fontSize: 60),
-        ),
-        ]
       ),
     );
   }
--- lib/modules/DraftTaskScreen.dart
+++ lib/modules/DraftTaskScreen.dart
@@ -1,15 +1,117 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_application/shared/componant/DataBaseClass.dart';
 
 class DraftTaskScreen extends StatelessWidget {
+
+  SqlDb dbObject =SqlDb();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Draft Tasks",
-          style: TextStyle(fontSize: 60),
-        ),
+      body:Column(
+
+          children:[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                const SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.lightBlue,
+                      child: IconButton(
+                        iconSize: 70,
+                        icon: Icon(Icons.visibility),
+                        onPressed: () async {
+                          print("view pressed");
+                          String sqlCommand = "SELECT * FROM 'TASKS'";
+                          var responce =
+                          await dbObject.readData(sqlCommand: sqlCommand);
+                          print("$responce");
+                        },
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.deepPurple,
+                      child: IconButton(
+                        iconSize: 70,
+                        icon: Icon(Icons.visibility),
+                        onPressed: () async {
+                          print("view pressed");
+                          String sqlCommand = "SELECT * FROM 'DoneTASKS'";
+                          var responce =
+                          await dbObject.readData(sqlCommand: sqlCommand);
+                          print("$responce");
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.lightBlue,
+                  child: IconButton(
+                    iconSize: 70,
+                    icon: Icon(Icons.remove_circle),
+                    onPressed: () async {
+                      print("delete pressed");
+                      String sqlCommand ="Delete FROM 'TASKS'";
+                      //   var responce =
+                      //await dbObject.DeleteData(sqlCommand: sqlCommand);
+
+                      //  print(responce);
+                    },
+                  ),
+                ),
+
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.lightBlue,
+                  child: IconButton(
+                    iconSize: 70,
+                    icon: Icon(Icons.delete),
+                    onPressed: ()  {
+
+                      dbObject.DeleteDataBase();
+
+                    },
+                  ),
+                ),
+              ],
+            ),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Text(
+              "Done Tasks",
+              style: TextStyle(fontSize: 60),
+            ),
+          ]
       ),
     );
   }
